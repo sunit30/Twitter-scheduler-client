77255f603b9ceab60ec12fd5f4bcd48b8c981a8a
Sunit <56727564+sunit30@users.noreply.github.com>
2023-03-06 22:19:47 +0530

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -1,7 +1,7 @@
 //import firebase from "firebase";
 import firebase from 'firebase/compat/app';
-//import 'firebase/compat/auth';
-//import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
 var firebaseConfig = {
   apiKey: "AIzaSyBlBqt_SDlYJLlCanwf1QYR7_5QQHwEL5I",
